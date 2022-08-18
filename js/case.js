46eccf58f18a956fed47b30792d523dc4bde988f
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-19 02:11:39 +0600

    case aadd

--- js/case.js
+++ js/case.js
@@ -1,10 +1,29 @@
-document.getElementById('btn-case-plus').addEventListener('click', function () {
+function updateCaseNumber(IsIncrease) {
     caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
 
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+    if (IsIncrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
 
-    console.log(previousCaseNumber);
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+   const newCaseNumber = updateCaseNumber(true);
+   const caseTotalPrice = newCaseNumber  * 59;
+   const caseTotalElement = document.getElementById('case-total');
+   caseTotalElement.innerText =caseTotalPrice;
+
+
+
+})
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    updateCaseNumber(false)
+
 })
