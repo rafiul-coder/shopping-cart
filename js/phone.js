7a268d08d7c9871b4cd8fbd6a532732ebfcc8199
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-21 02:34:53 +0600

    sub total start

--- js/phone.js
+++ js/phone.js
@@ -1,34 +1,43 @@
-function updatePhoneNumber(IsIncrease){
+function updatePhoneNumber(IsIncrease) {
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
 
     let newPhoneNumber;
-    if(IsIncrease){
+    if (IsIncrease) {
         newPhoneNumber = previousPhoneNumber + 1;
     }
-    else{
-        newPhoneNumber = previousPhoneNumber -1;
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
     }
     phoneNumberField.value = newPhoneNumber;
 
     return newPhoneNumber;
 }
 
-function updatePhoneTotalPrice(newPhoneNumber){
+function updatePhoneTotalPrice(newPhoneNumber) {
     const phoneTotalPrice = newPhoneNumber * 1219;
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice;
+}
+function getTextElementValueBuId(elementId) {
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
 }
 
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
     // updatePhoneNumber(true);
     const newPhoneNumber = updatePhoneNumber(true);
-   updatePhoneTotalPrice(newPhoneNumber);
+    updatePhoneTotalPrice(newPhoneNumber);
+    const currentPhoneTotal = getTextElementValueBuId('phone-total');
+    const currentCaseTotal = getTextElementValueBuId('case-total');
+    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
 })
 
-document.getElementById('btn-phone-minus').addEventListener('click',function(){
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
     // updatePhoneNumber(false);
-    const newPhoneNumber= updatePhoneNumber(false);
+    const newPhoneNumber = updatePhoneNumber(false);
     updatePhoneTotalPrice(newPhoneNumber);
 })
