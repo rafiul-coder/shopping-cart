6a306fc7efcbcd1380dd47e2ba45b2297865bb77
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-21 00:21:49 +0600

    phone side cleaar

--- js/phone.js
+++ js/phone.js
@@ -14,10 +14,21 @@ function updatePhoneNumber(IsIncrease){
 
     return newPhoneNumber;
 }
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
-    updatePhoneNumber(true);
+    // updatePhoneNumber(true);
+    const newPhoneNumber = updatePhoneNumber(true);
+   updatePhoneTotalPrice(newPhoneNumber);
 })
 
 document.getElementById('btn-phone-minus').addEventListener('click',function(){
-    updatePhoneNumber(false);
+    // updatePhoneNumber(false);
+    const newPhoneNumber= updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
 })
