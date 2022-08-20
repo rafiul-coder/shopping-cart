9da0f68b96fd4fa343a37e2cdf62c608744459e8
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-21 00:06:52 +0600

    phone plus miuos add

--- js/phone.js
+++ js/phone.js
@@ -1,8 +1,23 @@
-document.getElementById('btn-phone-plus').addEventListener('click', function () {
+function updatePhoneNumber(IsIncrease){
     const phoneNumberField = document.getElementById('phone-number-field');
     const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
 
-    const newPhoneNumber = previousPhoneNumber + 1;
+    let newPhoneNumber;
+    if(IsIncrease){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else{
+        newPhoneNumber = previousPhoneNumber -1;
+    }
     phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+    updatePhoneNumber(true);
+})
+
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+    updatePhoneNumber(false);
 })
